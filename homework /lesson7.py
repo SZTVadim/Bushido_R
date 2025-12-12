5debd71acf5dc4f9523cfb907061f70ef4fd1399
GasanoffR <168650104+GasanoffR@users.noreply.github.com>
2025-12-13 00:11:45 +0300

    исправил ошибку в задаче 1, добавил сумму в задаче 2

--- homework /lesson7.py
+++ homework /lesson7.py
@@ -1,5 +1,5 @@
 # Задача 1:
-numbers = [x**3 for x in range(1, 8)]
+numbers = [x**3 for x in range(1, 9)]
 print(numbers)
 print(min(numbers))
 print(max(numbers))
@@ -8,6 +8,7 @@ print(max(numbers))
 numbers2 = [5, 12, 8, 15, 3, 20, 7, 18, 9, 11]
 newlist = [x for x in numbers2 if x > 10]
 print(newlist)
+print(sum(newlist))
 
 # Задача 3
 cities = ["москва", "санкт-петербург", "казань"]
