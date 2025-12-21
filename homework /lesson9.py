26a82f2548cbebfd2c14ec5e82a3320b9598210c
GasanoffR <168650104+GasanoffR@users.noreply.github.com>
2025-12-21 23:12:16 +0300

    исправление комментариев

--- homework /lesson9.py
+++ homework /lesson9.py
@@ -12,7 +12,8 @@ for key in student:
     print(key)
 
 print("Вывод всех значений словаря:")
-print(student["name"], student["age"], student["course"], student["city"])
+for value in student.values():
+    print(value)
 
 print("используя цикл, вывести все пары:")
 for key, value in student.items():
@@ -34,9 +35,8 @@ print("Вывод сохранения переменной", param_remove,)
 print("Вывод удаления винограда", prices)
 
 prices = {"яблоко": 50, "банан": 30, "апельсин": 40, "груша": 35, "виноград": 60}
-for key, value in prices.items():
-    new_prices = value * 0.9
-    print("Вывод цен со скидкой 10%:", (f"{key}: {new_prices}"))
+new_prices = [f"{key}: {value * 0.9}" for key, value in prices.items()]
+print(new_prices)
 
 # ЗАДАНИЕ 3: Объединение словарей
 student1 = {"имя": "Иван", "возраст": 20, "курс": 2}
@@ -47,5 +47,6 @@ print(student1)
 student1 = {"имя": "Иван", "возраст": 20, "курс": 2}
 student2 = {"имя": "Мария", "возраст": 21, "город": "Санкт-Петербург"}
 student3 = {**student1, **student2}
+print(student1)
 print(student2)
 print(student3)
